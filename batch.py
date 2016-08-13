d93a2d9d4bdae68e5697b05187866b428965bfe4
Yeuh-Hua Tu <a504082002@gmail.com>
2016-08-14 03:08:15 +0800

    fix bug

--- batch.py
+++ batch.py
@@ -3,11 +3,11 @@ import os
 import multiprocessing
 
 proc_num = int(sys.argv[1])
-cmds = sys.argv[2:]
+cmds = map(lambda x: x.replace("'", ""), sys.argv[2:])
 
 pool = multiprocessing.Pool(proc_num)
 for cmd in cmds:
-    pool.apply(os.system, args=cmd)
+    pool.apply(os.system, args=(cmd, ))
 pool.close()
 pool.join()
 
