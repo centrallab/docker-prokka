ccd60dadc4e61bcb278f07cd43c234af9e879cd3
Yueh-Hua Tu <a504082002@gmail.com>
2017-03-22 16:22:56 +0800

    task 'add' is ready

--- app/tasks.py
+++ app/tasks.py
@@ -1,6 +1,7 @@
 from app.celeryapp import app
 import os
 import os.path
+import time
 
 def format_cmd(prog, flags, arg):
     a = list(map(lambda x: x[0] + "=" + str(x[1]) if x[1] != "" else x[0], flags))
@@ -8,9 +9,13 @@ def format_cmd(prog, flags, arg):
     return " ".join([prog, flag, arg])
 
 @app.task
+def add(x,y):
+    time.sleep(1)
+    return x+y
+
+@app.task
 def prokka(filename):
     name, ext = filename.split(".")
-
     args = list()
     args.append(("--cpus", "2"))
     args.append(("--outdir", os.path.join("/output", name)))
